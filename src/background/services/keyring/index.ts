b28b8a9dd17a2416c7991dcc77e2c6790dc7adc5
HOANG SON <hson91@Sons-MacBook-Pro.local>
2024-07-29 11:19:41 +0700

    sign tx with raw tx

--- src/background/services/keyring/index.ts
+++ src/background/services/keyring/index.ts
@@ -140,49 +140,52 @@ class KeyringService {
     const tx = params.tx;
     let txSkeleton = helpers.TransactionSkeleton();
     
-    if (tx.cellDeps && tx.cellDeps.length > 0) {
-      txSkeleton = txSkeleton.update("cellDeps", (cellDeps) =>cellDeps.push(...tx.cellDeps))
-    } else {
-      txSkeleton = txSkeleton.update("cellDeps", (cellDeps) =>
-        cellDeps.push({
+    if (tx.cell_deps && tx.cell_deps.length > 0) {
+      tx.cell_deps?.forEach((cellDep:any) => {
+        txSkeleton = txSkeleton.update('cellDeps', (cellDeps) => cellDeps.push({
           outPoint: {
-            txHash: (
-              (network.network as NetworkConfig).lumosConfig.SCRIPTS as any
-            ).SECP256K1_BLAKE160.TX_HASH,
-            index: (
-              (network.network as NetworkConfig).lumosConfig.SCRIPTS as any
-            ).SECP256K1_BLAKE160.INDEX,
+              txHash: cellDep.out_point.tx_hash,
+              index: cellDep.out_point.index,
           },
-          depType: (
-            (network.network as NetworkConfig).lumosConfig.SCRIPTS as any
-          ).SECP256K1_BLAKE160.DEP_TYPE,
-        })
-      );
-    }
-
-    if (tx.headerDeps) {
-      txSkeleton = txSkeleton.update("headerDeps", (headerDeps) =>
-        headerDeps.push(...tx.headerDeps)
-      )
-    }
-
-    if (tx.inputs) {
-      txSkeleton = txSkeleton.update("inputs", (inputs) =>
-        inputs.push(...tx.inputs)
-      )
-    }
-
-    if (tx.outputs) {
-      txSkeleton = txSkeleton.update("outputs", (outputs) =>
-        outputs.push(...tx.outputs)
-      )
-    }
-
-    if (tx.witnesses && tx.witnesses.length > 0) {
-      txSkeleton = txSkeleton.update("witnesses", (witnesses) =>
-        witnesses.push(...tx.witnesses)
-      )
+          depType: "depGroup"
+        }));
+      });
     } 
+
+    tx.inputs?.forEach((input: any) => {
+      txSkeleton = txSkeleton.update('inputs', (inputs) => inputs.push({
+        outPoint: {
+          txHash: input.previous_output.tx_hash,
+          index: input.previous_output.index,
+        },
+        data: input.output_data || "0x",
+        cellOutput: null,
+
+      }));
+  });
+
+  tx.outputs?.forEach((output: any, index: number) => {
+    txSkeleton = txSkeleton.update('outputs', (outputs) => outputs.push({
+      cellOutput: {
+        capacity: output.capacity,
+        lock: {
+          codeHash: output.lock?.code_hash,
+          hashType: output.lock?.hash_type,
+          args: output.lock?.args
+        },
+        type: output.type || null
+      },
+      data: "0x"
+    }));
+  });
+
+  tx.header_deps?.forEach((headerDep:any) => {
+      txSkeleton = txSkeleton.update('headerDeps', (headerDeps) => headerDeps.push(headerDep));
+  });
+
+  tx.witnesses?.forEach((witness:any) => {
+      txSkeleton = txSkeleton.update('witnesses', (witnesses) => witnesses.push(witness));
+  });
     
     const keyring = this.getKeyringByIndex(storageService.currentWallet.id);
     txSkeleton = commons.common.prepareSigningEntries(txSkeleton);
---
+++ src/shared/utils/num.ts
@@ -0,0 +1,176 @@
+import { Bytes, BytesLike, bytesConcat, bytesFrom } from "./bytes";
+import { Hex, HexLike, hexFrom } from "./hex";
+
+/**
+ * Represents a numeric value as a bigint.
+ */
+export type Num = bigint;
+
+/**
+ * Represents a value that can be converted to a numeric value.
+ * It can be a string, number, bigint, or HexLike.
+ */
+export type NumLike = string | number | bigint | HexLike;
+
+/**
+ * Get the max among all numbers.
+ *
+ * @param numbers - numbers.
+ * @returns The max numbers among them.
+ *
+ * @example
+ * ```typescript
+ * numMax(1, 2, 3); // Outputs 3n
+ * ```
+ */
+export function numMax(a: NumLike, ...numbers: NumLike[]): Num {
+  let max = numFrom(a);
+  numbers.forEach((nLike) => {
+    const n = numFrom(nLike);
+    if (n > max) {
+      max = n;
+    }
+  });
+  return max;
+}
+
+/**
+ * Converts a NumLike value to a Num (bigint).
+ *
+ * @param val - The value to convert, which can be a string, number, bigint, or HexLike.
+ * @returns A Num (bigint) representing the value.
+ *
+ * @example
+ * ```typescript
+ * const num = numFrom("12345"); // Outputs 12345n
+ * const numFromHex = numFrom("0x3039"); // Outputs 12345n
+ * ```
+ */
+export function numFrom(val: NumLike): Num {
+  if (typeof val === "bigint") {
+    return val;
+  }
+
+  if (typeof val === "string" || typeof val === "number") {
+    return BigInt(val);
+  }
+
+  return BigInt(hexFrom(val));
+}
+
+/**
+ * Converts a NumLike value to a hexadecimal string.
+ *
+ * @param val - The value to convert, which can be a string, number, bigint, or HexLike.
+ * @returns A Hex string representing the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const hex = numToHex(12345); // Outputs "0x3039"
+ * ```
+ */
+export function numToHex(val: NumLike): Hex {
+  return `0x${numFrom(val).toString(16)}`;
+}
+
+/**
+ * Converts a NumLike value to a byte array in little-endian order.
+ *
+ * @param val - The value to convert, which can be a string, number, bigint, or HexLike.
+ * @param bytes - The number of bytes to use for the representation. If not provided, the exact number of bytes needed is used.
+ * @returns A Uint8Array containing the byte representation of the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const bytes = numToBytes(12345, 4); // Outputs Uint8Array [57, 48, 0, 0]
+ * ```
+ */
+
+export function numToBytes(val: NumLike, bytes?: number): Bytes {
+  return numLeToBytes(val, bytes);
+}
+
+/**
+ * Converts a NumLike value to a byte array in little-endian order.
+ *
+ * @param val - The value to convert, which can be a string, number, bigint, or HexLike.
+ * @param bytes - The number of bytes to use for the representation. If not provided, the exact number of bytes needed is used.
+ * @returns A Uint8Array containing the byte representation of the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const bytes = numLeToBytes(12345, 4); // Outputs Uint8Array [57, 48, 0, 0]
+ * ```
+ */
+export function numLeToBytes(val: NumLike, bytes?: number): Bytes {
+  return numBeToBytes(val, bytes).reverse();
+}
+
+/**
+ * Converts a NumLike value to a byte array in big-endian order.
+ *
+ * @param val - The value to convert, which can be a string, number, bigint, or HexLike.
+ * @param bytes - The number of bytes to use for the representation. If not provided, the exact number of bytes needed is used.
+ * @returns A Uint8Array containing the byte representation of the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const bytes = numBeToBytes(12345, 4); // Outputs Uint8Array [0, 0, 48, 57]
+ * ```
+ */
+export function numBeToBytes(val: NumLike, bytes?: number): Bytes {
+  const rawBytes = bytesFrom(numFrom(val).toString(16));
+  if (bytes == null) {
+    return rawBytes;
+  }
+
+  return bytesConcat(
+    Array.from(Array(bytes - rawBytes.length), () => 0),
+    rawBytes,
+  );
+}
+
+/**
+ * Converts a byte array to a Num (bigint) assuming little-endian order.
+ *
+ * @param val - The byte array to convert.
+ * @returns A Num (bigint) representing the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const num = numFromBytes(new Uint8Array([57, 48, 0, 0])); // Outputs 12345n
+ * ```
+ */
+export function numFromBytes(val: BytesLike): Num {
+  return numLeFromBytes(val);
+}
+
+/**
+ * Converts a byte array to a Num (bigint) assuming little-endian order.
+ *
+ * @param val - The byte array to convert.
+ * @returns A Num (bigint) representing the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const num = numLeFromBytes(new Uint8Array([57, 48, 0, 0])); // Outputs 12345n
+ * ```
+ */
+export function numLeFromBytes(val: BytesLike): Num {
+  return numBeFromBytes(bytesFrom(val).reverse());
+}
+
+/**
+ * Converts a byte array to a Num (bigint) assuming big-endian order.
+ *
+ * @param val - The byte array to convert.
+ * @returns A Num (bigint) representing the numeric value.
+ *
+ * @example
+ * ```typescript
+ * const num = numBeFromBytes(new Uint8Array([0, 0, 48, 57])); // Outputs 12345n
+ * ```
+ */
+export function numBeFromBytes(val: BytesLike): Num {
+  return numFrom(bytesFrom(val));
+}
