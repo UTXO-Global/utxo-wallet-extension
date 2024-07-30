8a80fc112c173d5310eaa773745467e40f80ddb7
HOANG SON <hson91@Sons-MacBook-Pro.local>
2024-07-30 20:45:09 +0700

    add ccc

--- src/background/services/keyring/ckbhdw/hd/hdSeedKey.ts
+++ src/background/services/keyring/ckbhdw/hd/hdSeedKey.ts
@@ -23,10 +23,8 @@ import type {
   ToSignInput,
 } from "./types";
 import { isTaprootInput, toXOnly } from "./utils";
-import { bytesConcat, bytesFrom } from "@/shared/utils/bytes";
-import { hexFrom } from "@/shared/utils/hex";
-import { numBeToBytes } from "@/shared/utils/num";
 import { messageHashCkbSecp256k1 } from ".";
+import { ccc } from "@ckb-ccc/core";
 
 const ECPair = ECPairFactory(tinysecp);
 
@@ -152,16 +150,16 @@ class HDSeedKey implements Keyring<SerializedHDKey> {
     const account = this.getAccountByPath(hdPath);
     if (["nervos", "nervos_testnet"].includes(networkSlug)) {
       const signature = secp256k1.sign(
-        bytesFrom(messageHashCkbSecp256k1(text)),
-        bytesFrom(hexFrom(account.privateKey))
+        ccc.bytesFrom(messageHashCkbSecp256k1(text)),
+        ccc.bytesFrom(ccc.hexFrom(account.privateKey))
       );
       const { r, s, recovery } = signature;
 
-      return hexFrom(
-        bytesConcat(
-          numBeToBytes(r, 32),
-          numBeToBytes(s, 32),
-          numBeToBytes(recovery, 1)
+      return ccc.hexFrom(
+        ccc.bytesConcat(
+          ccc.numBeToBytes(r, 32),
+          ccc.numBeToBytes(s, 32),
+          ccc.numBeToBytes(recovery, 1)
         )
       );
     }
--- src/background/services/keyring/ckbhdw/hd/index.ts
+++ src/background/services/keyring/ckbhdw/hd/index.ts
@@ -1,9 +1,7 @@
-import { BytesLike, bytesFrom } from "@/shared/utils/bytes";
+import { ccc } from "@ckb-ccc/core";
 import HDPrivateKey from "./hdPrivateKey";
 import HDSeedKey from "./hdSeedKey";
 import { AddressType, Keyring } from "./types";
-import { Hex, hexFrom } from "@/shared/utils/hex";
-import { hashCkb } from "@/shared/utils/hasher";
 
 export async function fromMnemonic(mnemonic: string): Promise<HDSeedKey> {
   return await HDSeedKey.fromMnemonic({ mnemonic });
@@ -16,10 +14,12 @@ export function fromPrivateKey(privateKey: Uint8Array): HDPrivateKey {
 export * as types from "./types";
 export { default as englishWords } from "./words/english";
 
-export function messageHashCkbSecp256k1(message: string | BytesLike): Hex {
-  const msg = typeof message === "string" ? message : hexFrom(message);
-  const buffer = bytesFrom(`Nervos Message:${msg}`, "utf8");
-  return hashCkb(buffer);
+export function messageHashCkbSecp256k1(
+  message: string | ccc.BytesLike
+): ccc.Hex {
+  const msg = typeof message === "string" ? message : ccc.hexFrom(message);
+  const buffer = ccc.bytesFrom(`Nervos Message:${msg}`, "utf8");
+  return ccc.hashCkb(buffer);
 }
 
 export { AddressType, HDPrivateKey, HDSeedKey, Keyring };
