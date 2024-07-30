f59f49ea93620bad2cfae2666237ebe4c3c0ebab
yangiv <khoahua.yiv@gmail.com>
2024-07-30 16:39:53 +0700

    fix: native coin price

--- src/ui/pages/main/wallet/account-panel/component.tsx
+++ src/ui/pages/main/wallet/account-panel/component.tsx
@@ -198,7 +198,7 @@ const AccountPanel = () => {
               </div>
               <div className="text-center">
                 {currentAccount?.balance !== undefined
-                  ? currentPrice !== undefined && (
+                  ? currentPrice !== 0 && (
                       <div className="text-[#787575] gap-[5px] font-normal text-lg leading-[25.2px] flex items-center justify-center">
                         <IcnApproximate className="w-[9px]" /> $
                         {(currentAccount.balance * currentPrice)?.toFixed(3)}
