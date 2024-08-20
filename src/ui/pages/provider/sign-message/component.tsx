ee3d65d1707ab544d27a159cd4305f8edc48effa
HOANG SON <hson91@sons-mbp.0.10.168.192.in-addr.arpa>
2024-08-20 12:07:21 +0700

    fix cw268: add account to popup sign

--- src/ui/pages/provider/sign-message/component.tsx
+++ src/ui/pages/provider/sign-message/component.tsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 
 import Layout from "../layout";
 import { t } from "i18next";
+import { useGetCurrentAccount } from "@/ui/states/walletState";
+import { shortAddress } from "@/shared/utils/transactions";
 
 const SignMessage = () => {
   const [message, setMessage] = useState<string>();
-
+  const currentAccount = useGetCurrentAccount();
   const { notificationController } = useControllersState((v) => ({
     notificationController: v.notificationController,
   }));
@@ -25,21 +27,32 @@ const SignMessage = () => {
       resolveBtnClassName="text-text bg-primary hover:bg-[#E37800]"
       resolveBtnText={t("provider.sign")}
     >
-      <>
+      <div className="-mt-5">
         <img
           src="/sign-message.png"
-          alt="sign message"
           className="w-[116px] mx-auto"
+          alt="sign message"
         />
-        <h3 className="text-[24px] leading-[28px] font-medium text-primary text-center capitalize mt-6">
+        <h3 className="text-[24px] leading-[28px] font-medium text-primary text-center capitalize mt-4">
           {t("provider.sign_request")}
         </h3>
-        <div className="px-4 py-[20px] rounded-lg bg-[#F5F5F5] mt-10">
+        <div className="px-4 py-[10px] rounded-lg bg-[#F5F5F5] mt-8 flex items-center gap-3">
+          <img src="account.png" className="w-8 h-8" />
+          <div className="break-words whitespace-pre-wrap overflow-y-auto text-sm flex flex-col gap-[2px]">
+            <strong className="text-lg font-medium text-primary leading-6">
+              {currentAccount.name}
+            </strong>
+            <span className="text-[#787575]">
+              {shortAddress(currentAccount.accounts[0].address, 12)}
+            </span>
+          </div>
+        </div>
+        <div className="px-4 py-[20px] rounded-lg bg-[#F5F5F5] mt-4">
           <div className="break-words whitespace-pre-wrap max-h-60 overflow-y-auto text-primary text-base">
             {message}
           </div>
         </div>
-      </>
+      </div>
     </Layout>
   );
 };
