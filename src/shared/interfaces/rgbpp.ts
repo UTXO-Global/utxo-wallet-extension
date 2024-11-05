64d2aa122f68d43f15595d448789f3fd188d7a8f
violet <lvhoa58@gmail.com>
2024-11-05 11:07:09 +0700

    add rgbpp lib

---
+++ src/shared/interfaces/rgbpp.ts
@@ -0,0 +1,124 @@
+import { Script } from "@ckb-lumos/base";
+import { NetworkSlug } from "../networks/types";
+
+export function getRgbppAssetApiUrl(networkSlug: NetworkSlug): string {
+  switch (networkSlug) {
+    case "btc":
+    //   return "https://api.rgbpp.io";
+    case "btc_testnet":
+    //   return "https://api.testnet.rgbpp.io";
+    case "btc_signet":
+      //   return "https://api.signet.rgbpp.io";
+      return "http://localhost:3000";
+    default:
+      throw Error(`Invalid network slug: ${networkSlug}`);
+  }
+}
+
+export interface RgbppAsset {
+  cellOutput: {
+    capacity: string;
+    lock: {
+      codeHash: string;
+      args: string;
+      hashType: string;
+    };
+    type: {
+      codeHash: string;
+      args: string;
+      hashType: string;
+    };
+  };
+  data: string;
+  balance: number;
+  outPoint: {
+    txHash: string;
+    index: string;
+  };
+  blockNumber: string;
+  txIndex: string;
+  typeHash: string;
+}
+
+const fromHexString = (hexString: string) =>
+  Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+
+const toHexString = (bytes: Uint8Array) =>
+  bytes.reduce(
+    (str: string, byte: number) => str + byte.toString(16).padStart(2, "0"),
+    ""
+  );
+
+export function getRgbppBtcOutpoint(cell: RgbppAsset) {
+  const args = cell.cellOutput.lock.args;
+  const tx = toHexString(fromHexString(args.slice(10)).reverse());
+  const outIndex = parseInt(
+    toHexString(fromHexString(args.slice(2, 10)).reverse()),
+    16
+  );
+  return {
+    tx,
+    outIndex,
+  };
+}
+
+export interface RgbppXudtBalance {
+  symbol: string;
+  name: string;
+  decimal: number;
+  type_hash: string;
+  type_script: {
+    codeHash: string;
+    args: string;
+    hashType: string;
+  };
+  total_amount: string;
+  available_amount: string;
+  pending_amount: string;
+}
+
+export function xudtBalanceFormat(xudt: RgbppXudtBalance) {
+  return parseInt(xudt.available_amount, 16) / Math.pow(10, xudt.decimal);
+}
+
+export interface RgbppTx {
+  btcTx: {
+    txid: string;
+    vin: {
+      txid: string;
+      vout: number;
+      prevout: {
+        scriptpubkey_address: string;
+        value: number;
+      };
+    }[];
+    vout: {
+      scriptpubkey_address: string;
+      value: number;
+    }[];
+    status: {
+      confirmed: boolean;
+      block_height: number;
+      block_time: number;
+    };
+  };
+  isomorphicTx: {
+    ckbTx: {
+      hash: string;
+      outputsData: string[];
+    };
+    inputs: {
+      capacity: string;
+      lock: Script;
+      type: Script;
+    }[];
+    outputs: {
+      capacity: string;
+      lock: Script;
+      type: Script;
+    }[];
+    status: {
+      confirmed: true;
+    };
+  };
+}
---
+++ src/ui/pages/main/rgbpp-detail/component.tsx
@@ -0,0 +1,273 @@
+import {
+  useGetCurrentAccount,
+  useGetCurrentNetwork,
+} from "@/ui/states/walletState";
+import { useEffect, useMemo, useState } from "react";
+import Loading from "react-loading";
+import { IcnCopy, IcnReceive, IcnSend } from "@/ui/components/icons";
+import toast from "react-hot-toast";
+import { t } from "i18next";
+import cn from "classnames";
+import Analytics from "@/ui/utils/gtm";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import ReceiveAddress from "@/ui/components/receive-address";
+import { ckbExplorerApi } from "@/ui/utils/helpers";
+import { CKBTokenInfo } from "@/shared/networks/ckb/types";
+import { shortAddress } from "@/shared/utils/transactions";
+import ShortBalance from "@/ui/components/ShortBalance";
+import TextAvatar from "@/ui/components/text-avatar/component";
+import DOMPurify from "dompurify";
+import { CKB_MAINNET, CKB_TESTNET } from "@/shared/networks/ckb";
+import { BTC_LIVENET } from "@/shared/networks/btc";
+import RgbppTxList from "./rgbpp-tx-list";
+import { calculateScriptPack, toHexString } from "@/background/utils";
+
+const RgbppDetail = () => {
+  const { typeHash } = useParams();
+  const [isShowReceive, setIsShowReceive] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [tokenInfo, setTokenInfo] = useState<CKBTokenInfo | undefined>(
+    undefined
+  );
+  const currentAccount = useGetCurrentAccount();
+  const currentNetwork = useGetCurrentNetwork();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const tokenBalance = useMemo(() => {
+    if (location.state && location.state.tokenAmount) {
+      return location.state.tokenAmount;
+    }
+    return 0;
+  }, [location]);
+
+  const typeScriptString = useMemo(() => {
+    if (tokenInfo) {
+      const { code_hash, hash_type, args } = tokenInfo.attributes.type_script;
+      return toHexString(
+        calculateScriptPack({
+          codeHash: code_hash,
+          hashType: hash_type as any,
+          args: args,
+        })
+      );
+    }
+  }, [tokenInfo]);
+
+  const getToken = async () => {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    try {
+      const ckbSlug =
+        currentNetwork.slug === BTC_LIVENET.slug
+          ? CKB_MAINNET.slug
+          : CKB_TESTNET.slug;
+      const res = await fetch(
+        `${ckbExplorerApi(ckbSlug)}/v1/xudts/${typeHash}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/vnd.api+json",
+          },
+        }
+      );
+      const { data } = await res.json();
+      setTokenInfo(data as CKBTokenInfo);
+    } catch (e) {
+      console.error(e);
+    }
+    setIsLoading(false);
+  };
+
+  const panelNavs = useMemo(() => {
+    return [
+      {
+        navPath: "/pages/receive",
+        navName: "pf_receive",
+        navLabel: "receive",
+        icon: <IcnReceive />,
+        title: t("wallet_page.receive"),
+        isPopup: true,
+      },
+      {
+        navPath: "/pages/create-send-rgbpp",
+        navName: "pf_send",
+        navLabel: "send",
+        icon: <IcnSend />,
+        title: t("wallet_page.send"),
+      },
+    ];
+  }, [currentAccount.accounts]);
+
+  const fullNameLength = useMemo(() => {
+    const fullnameLen = tokenInfo?.attributes?.full_name?.length || 0;
+    const symbolLen = tokenInfo?.attributes?.symbol?.length || 0;
+    return fullnameLen + symbolLen;
+  }, [tokenInfo]);
+
+  const _navigate = (path: string, name: string, label: string) => {
+    // NOTE: [GA]
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    Analytics.fireEvent(name, {
+      action: "click",
+      label,
+    });
+    navigate(path, {
+      state: {
+        token: tokenInfo ? tokenInfo : undefined,
+      },
+    });
+  };
+
+  useEffect(() => {
+    getToken().catch((e) => {
+      console.log(e);
+    });
+  }, []);
+
+  if (!currentAccount || !tokenInfo) return <Loading color="#ODODOD" />;
+
+  return (
+    <>
+      <div className="flex gap-4 items-center bg-grey-400 px-4 py-2 w-full h-[92px]">
+        {!!tokenInfo.attributes.icon_file ? (
+          <img
+            src={DOMPurify.sanitize(tokenInfo.attributes.icon_file)}
+            className="w-16 h-16"
+          />
+        ) : (
+          <TextAvatar
+            len={2}
+            text={tokenInfo.attributes.symbol}
+            className="!w-16 !h-16 !text-3xl"
+          />
+        )}
+        <div className="flex flex-col gap-1 flex-grow">
+          <div
+            className={cn("text-primary flex gap-1 items-center w-full", {
+              "flex-col !items-start": fullNameLength > 10,
+            })}
+          >
+            <div className="flex items-center gap-1">
+              <strong className="font-bold text-xl leading-[25px]">
+                {!!tokenInfo.attributes.symbol
+                  ? tokenInfo.attributes.symbol
+                  : "Unnamed"}
+              </strong>
+              {!!tokenInfo.attributes.full_name && (
+                <div className="text-base font-normal leading-[25px] flex gap-[3px] items-center">
+                  <span className="-mt-[3px]">(</span>
+                  <span>{tokenInfo.attributes.full_name}</span>{" "}
+                  <span className="-mt-[3px]">)</span>
+                </div>
+              )}
+            </div>
+            {tokenInfo && (
+              <div
+                className={cn("pb-[1px] flex items-center", {
+                  "mt-1": fullNameLength > 10,
+                })}
+              >
+                {fullNameLength <= 10 && (
+                  <div className="h-[21px] w-[1px] bg-grey-200" />
+                )}
+                <div className="flex gap-1 items-start">
+                  <label className="inline-block bg-grey-300 px-2 rounded text-[10px] text-[#787575] leading-5">
+                    {tokenInfo.attributes.udt_type.toUpperCase()}
+                  </label>
+                  {tokenInfo.attributes.xudt_tags?.includes("rgb++") && (
+                    <label
+                      className="inline-block bg-grey-300 px-2 rounded text-[10px] text-black leading-5"
+                      style={{
+                        background:
+                          "linear-gradient(120.76deg, #FFD37B 31.34%, #77F8BA 92.54%)",
+                      }}
+                    >
+                      RGB++
+                    </label>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+          {
+            <div className="text-sm font-normal leading-[18px] text-[#787575] flex gap-2">
+              <p>{shortAddress(tokenInfo.attributes.type_hash, 8)}</p>
+              <IcnCopy
+                className="w-4 h-4 right-2 transition-colors stroke-[#787575] hover:stroke-primary cursor-pointer"
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(tokenInfo.attributes.type_hash)
+                    .then(() => {
+                      toast.success("Type hash copied");
+                    })
+                    .catch((err) => {
+                      toast.error("Failed to copy");
+                    });
+                }}
+              />
+            </div>
+          }
+        </div>
+      </div>
+      <div className="relative w-full h-full top-0 px-4">
+        <div className="w-full flex-col justify-start relative top-0 overflow-auto">
+          <div className="flex flex-col gap-1 mt-4">
+            <div className="font-medium leading-6 flex gap-4 justify-between items-center">
+              <label className="text-base">
+                {t("components.token_card.balance")}
+              </label>
+              <ShortBalance balance={tokenBalance} zeroDisplay={18} />
+            </div>
+          </div>
+
+          <div
+            className={cn(`grid gap-2 mt-4`, {
+              "grid-cols-2": panelNavs.length <= 2,
+              "grid-cols-4": panelNavs.length > 2,
+            })}
+          >
+            {panelNavs.map((nav, i) => (
+              <div
+                key={`panel-account-nav-${i}`}
+                className={cn(
+                  `py-3 px-4 flex gap-1 flex-col cursor-pointer justify-center items-center transition-all bg-grey-300 hover:bg-grey-200 rounded-[10px]`,
+                  {
+                    "!cursor-not-allowed hover:!bg-grey-300":
+                      tokenBalance <= 0 &&
+                      nav.navPath === "/pages/create-send-rgbpp",
+                  }
+                )}
+                onClick={() => {
+                  if (nav.isPopup) {
+                    setIsShowReceive(true);
+                  } else {
+                    if (
+                      tokenBalance <= 0 &&
+                      nav.navPath === "/pages/create-send-rgbpp"
+                    )
+                      return;
+                    _navigate(nav.navPath, nav.navName, nav.navLabel);
+                  }
+                }}
+              >
+                <p className="text-base font-medium">{nav.title}</p>
+              </div>
+            ))}
+          </div>
+          <RgbppTxList
+            className="mt-4 mb-4 !px-0 !z-5"
+            typeScriptString={typeScriptString}
+          />
+        </div>
+        <ReceiveAddress
+          active={isShowReceive}
+          onClose={() => setIsShowReceive(false)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default RgbppDetail;
