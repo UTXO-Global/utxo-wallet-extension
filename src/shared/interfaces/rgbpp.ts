fdbbd3656edabe51fa32b2fd82bb6980046f279f
violet <lvhoa58@gmail.com>
2024-11-12 21:09:10 +0700

    rgbpp wip

--- src/shared/interfaces/rgbpp.ts
+++ src/shared/interfaces/rgbpp.ts
@@ -6,7 +6,8 @@ export function getRgbppAssetApiUrl(networkSlug: NetworkSlug): string {
     case "btc":
     //   return "https://api.rgbpp.io";
     case "btc_testnet":
-    //   return "https://api.testnet.rgbpp.io";
+      //   return "https://api.testnet.rgbpp.io";
+      return "http://localhost:3001";
     case "btc_signet":
       //   return "https://api.signet.rgbpp.io";
       return "http://localhost:3000";
